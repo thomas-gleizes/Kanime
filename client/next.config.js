21aa3b28eeafee265fa9d0240b58c1af703dcd50
Thomas <thomasgleizes34@gmail.com>
2021-10-23 11:13:10 +0200

    swap index redirect to next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -3,4 +3,13 @@ module.exports = {
   images: {
     domains: ["media.kitsu.io"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/explore/animes",
+        permanent: true,
+      },
+    ];
+  },
 };
