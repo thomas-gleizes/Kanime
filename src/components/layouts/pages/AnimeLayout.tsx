696dbc306b63a5807d3d04ff20b84bd8ed26a795
Thomas <thomasgleizes34@gmail.com>
2022-03-06 22:55:39 +0100

    fix anime layout

--- src/components/layouts/pages/AnimeLayout.tsx
+++ src/components/layouts/pages/AnimeLayout.tsx
@@ -12,7 +12,7 @@ import Title from 'components/layouts/Title';
 import KitsuButton from 'components/common/KitsuButton';
 import Button from 'components/common/Button';
 import EditAnimesEntries from 'components/modal/EditAnimesEntries';
-import Layout from 'components/layouts/Layout';
+import Header from 'components/layouts/Header';
 
 export interface AnimeLayoutProps {
   anime: Anime;
@@ -68,69 +68,72 @@ const AnimeLayout: Component<AnimeLayoutProps & { children: NodeR }> = ({
   if (error) return <Error statusCode={error.code} title={error.message} />;
 
   return (
-    <Layout>
+    <>
       <Title>{anime.canonicalTitle}</Title>
-      <div className="min-h-screen pb-16">
-        <div className="w-full">
-          <div className="relative w-full h-[450px]">
-            <div className="fixed -z-10 w-full h-[450px] bg-primary-dark">
-              <div
-                className="w-full h-full bg-cover bg-center bg-no-repeat"
-                style={{ backgroundImage: `url('${anime.cover?.small}')` }}
-              >
-                <div className="w-full h-full bg-black bg-opacity-40" />
+      <Header />
+      <main>
+        <div className="min-h-screen pb-16">
+          <div className="w-full">
+            <div className="relative w-full h-[450px]">
+              <div className="fixed -z-10 w-full h-[450px] bg-primary-dark">
+                <div
+                  className="w-full h-full bg-cover bg-center bg-no-repeat"
+                  style={{ backgroundImage: `url('${anime.cover?.small}')` }}
+                >
+                  <div className="w-full h-full bg-black bg-opacity-40" />
+                </div>
               </div>
             </div>
+            <nav className="relative bg-white p-0 w-full">
+              <div className="flex justify-center divide-x-2 divide-gray-200">
+                {TABS.map((tab, index) => (
+                  <NavLink
+                    key={index}
+                    href={`${routes.animes.list}/${anime.slug}${tab.path}`}
+                  >
+                    {tab.label}
+                  </NavLink>
+                ))}
+              </div>
+            </nav>
           </div>
-          <nav className="relative bg-white p-0 w-full">
-            <div className="flex justify-center divide-x-2 divide-gray-200">
-              {TABS.map((tab, index) => (
-                <NavLink
-                  key={index}
-                  href={`${routes.animes.list}/${anime.slug}${tab.path}`}
-                >
-                  {tab.label}
-                </NavLink>
-              ))}
-            </div>
-          </nav>
-        </div>
-        <div className="w-full bg-gray-50 z-100">
-          <div className="mx-auto w-full max-w-[1150px]">
-            <div className="sticky float-right w-200 top-[230px]">
-              <div className="relative w-[214px] top-[-150px]">
-                <div className="w-full shadow h-[304px] bg-kitsu mb-2">
-                  {anime.poster?.small && (
-                    <Image
-                      src={anime.poster.small as string}
-                      width={214}
-                      height={304}
-                      alt="test"
-                    />
-                  )}
-                </div>
-                <div className="border w-full border-gray-200 p-2">
-                  <div className="flex flex-col space-y-2">
-                    <div>
-                      <KitsuButton slug={anime.slug} />
-                    </div>
-                    <div>
-                      <Button outline color="amber" onClick={toggleModal}>
-                        Ajouter
-                      </Button>
+          <div className="w-full bg-gray-50 z-100">
+            <div className="mx-auto w-full max-w-[1150px]">
+              <div className="sticky float-right w-200 top-[230px]">
+                <div className="relative w-[214px] top-[-150px]">
+                  <div className="w-full shadow h-[304px] bg-kitsu mb-2">
+                    {anime.poster?.small && (
+                      <Image
+                        src={anime.poster.small as string}
+                        width={214}
+                        height={304}
+                        alt="test"
+                      />
+                    )}
+                  </div>
+                  <div className="border w-full border-gray-200 p-2">
+                    <div className="flex flex-col space-y-2">
+                      <div>
+                        <KitsuButton slug={anime.slug} />
+                      </div>
+                      <div>
+                        <Button outline color="amber" onClick={toggleModal}>
+                          Ajouter
+                        </Button>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="mr-[210px]">
-              <div className="w-full py-5">{children}</div>
+              <div className="mr-[210px]">
+                <div className="w-full py-5">{children}</div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </main>
       <EditAnimesEntries anime={anime} isOpen={openModal} toggle={toggleModal} />
-    </Layout>
+    </>
   );
 };
 
