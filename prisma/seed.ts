eea8228ead27c84995af140aa237227afa61ba63
Thomas <thomasgleizes34@gmail.com>
2022-03-22 21:24:43 +0100

    replace faker by @faker-js/faker

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client';
-import faker from 'faker';
+import faker from '@faker-js/faker';
 
 import { defaultUsersMedia } from 'resources/constants';
 import Security from '../src/services/security.service';
