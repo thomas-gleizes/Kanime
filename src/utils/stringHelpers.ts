e1514ac65c82623c8700abf29b738412bb13b28d
Thomas <thomasgleizes34@gmail.com>
2022-03-25 16:12:49 +0100

    add string helpers with copilot

--- src/utils/stringHelpers.ts
+++ src/utils/stringHelpers.ts
@@ -21,3 +21,35 @@ export function camelCase(str: string): string {
     })
     .replace(/\s/g, '');
 }
+
+export function kebabCase(str: string): string {
+  return str.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+}
+
+export function snakeCase(str: string): string {
+  return str.replace(/([a-z])([A-Z])/g, '$1_$2').toLowerCase();
+}
+
+export function isEmpty(str: string): boolean {
+  return str.length === 0;
+}
+
+export function isNotEmpty(str: string): boolean {
+  return str.length > 0;
+}
+
+export function isBlank(str: string): boolean {
+  return str.trim().length === 0;
+}
+
+export function isNotBlank(str: string): boolean {
+  return str.trim().length > 0;
+}
+
+export function isNullOrEmpty(str: string): boolean {
+  return str === null || str === '';
+}
+
+export function isNullOrBlank(str: string): boolean {
+  return str === null || str.trim().length === 0;
+}
