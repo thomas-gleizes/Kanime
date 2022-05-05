f015254061a340ce911349a32ca52a1c3fb8f747
Thomas <thomasgleizes34@gmail.com>
2022-05-05 16:04:56 +0200

    try swr

--- src/pages/dev.page.tsx
+++ src/pages/dev.page.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
-import EmptyLayout from 'components/layouts/pages/EmptyLayout';
+import React, { useEffect } from 'react';
+import useSWR, { SWRConfig } from 'swr';
 
 const DevPage = ({ ...props }) => {
+  const { data } = useSWR('/api/animes/2');
+
+  useEffect(() => console.log('Data', data), [data]);
+
   return <></>;
 };
 
-DevPage.layout = EmptyLayout;
+DevPage.layout = ({ children }) => {
+  return (
+    <SWRConfig
+      value={{
+        refreshInterval: 3000,
+        fetcher: (resource, init) => {
+          console.log('Resource', resource);
+          console.log('Init', init);
+
+          return fetch(resource, init).then((res) => res.json());
+        },
+      }}
+    >
+      {children}
+    </SWRConfig>
+  );
+};
 
 export default DevPage;
