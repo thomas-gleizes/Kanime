b2119db84bc0742d1c1e0ab2361c976c1ea8893b
Thomas <thomasgleizes34@gmail.com>
2022-05-31 16:01:41 +0200

    fix user entries

--- src/pages/api/users/[id]/entries.api.ts
+++ src/pages/api/users/[id]/entries.api.ts
@@ -1,7 +1,6 @@
+import { Visibility } from '@prisma/client';
 import { ApiRequest, ApiResponse } from 'next/app';
-import { Visibility } from 'app/model';
 
-import { PrismaVisibility } from 'resources/prisma';
 import { apiHandler } from 'services/handler.service';
 import { withSessionApi } from 'services/session.service';
 import { EntryModel, UserFollowModel } from 'models';
@@ -13,17 +12,16 @@ handler.get(async (req: ApiRequest, res: ApiResponse<any>) => {
   const { id } = req.query;
   const { user } = req.session;
 
-  const visibility: Visibility[] = PrismaVisibility.public as Visibility;
+  const visibility: Visibility[] = ['public'];
   if (user.id)
-    if (user.id === +id)
-      visibility.push(PrismaVisibility.limited, PrismaVisibility.private);
+    if (user.id === +id) visibility.push('limited', 'private');
     else {
       const [one, two] = await Promise.all([
         UserFollowModel.isFollow(user.id, +id),
         UserFollowModel.isFollow(+id, user.id),
       ]);
 
-      if (one && two) visibility.push(PrismaVisibility.limited);
+      if (one && two) visibility.push('limited');
     }
 
   const entries = EntriesMapper.many(await EntryModel.getByUser(+id, visibility));
