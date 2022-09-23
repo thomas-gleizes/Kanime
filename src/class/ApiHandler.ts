eaaa8f7dd69c19dab1305861c925ebf03bbad791
Thomas <thomasgleizes34@gmail.com>
2022-09-23 16:03:31 +0200

    global errors handling

--- src/class/ApiHandler.ts
+++ src/class/ApiHandler.ts
@@ -1,6 +1,18 @@
-import { UseMiddleware } from 'next-api-decorators';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { Catch, HttpException, UseMiddleware } from 'next-api-decorators';
 import { apiLogger } from 'middlewares/logger.middleware';
 import queryParser from 'middlewares/queryParser.middleware';
 
+function exceptionHandler(error: unknown, req: NextApiRequest, res: NextApiResponse) {
+  if (error instanceof HttpException) res.status(error.statusCode).json(error);
+
+  if (process.env.NODE_ENV !== 'production') {
+    res.status(500).json(error);
+  }
+
+  res.status(500).json({ message: 'Internal Server Error' });
+}
+
 @UseMiddleware(queryParser, apiLogger)
+@Catch(exceptionHandler)
 export default abstract class ApiHandler {}
